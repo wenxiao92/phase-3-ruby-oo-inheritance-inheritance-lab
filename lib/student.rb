7a6355c066f806fa57c82d125d9ad2d89d847339
Wen Xiao <wennstyle@gmail.com>
2022-06-10 22:47:19 -0400

    commit tree fe8c065e8752c0a0b84c365a5a28c9e42be47be8

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(a_string)
+        knowledge << a_string
+    end
 
 end
